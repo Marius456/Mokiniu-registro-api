67339c0c7a99a7f08883df3dc44cb7d5fd4f473e
Marius <marius.zilguzis@ktu.edu>
2023-02-22 10:54:14 +0200

    added models(child, parent, school)

---
+++ Mokiniu registro api/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mokiniu_registro_api.Models;
+using System.Reflection;
+
+namespace Mokiniu_registro_api
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+           : base(options)
+        { }
+
+        public DbSet<Parent> Parents { get; set; }
+        public DbSet<Child> Children { get; set; }
+        public DbSet<School> Schools { get; set;}
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
