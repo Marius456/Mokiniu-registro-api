e9c758b8ba624936f4bd4aa3264a820fd7f663ad
Marius <marius.zilguzis@ktu.edu>
2023-02-26 14:36:50 +0200

    added ChildrenController responses

---
+++ Mokiniu registro api/DTOs/Errors/Error.cs
@@ -0,0 +1,7 @@
+﻿namespace Mokiniu_registro_api.DTOs.Errors
+{
+    public class Error
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ Mokiniu registro api/Services/Responses/ChildrenResponse.cs
@@ -0,0 +1,26 @@
+﻿using Mokiniu_registro_api.Models;
+
+namespace Mokiniu_registro_api.Services.Responses
+{
+    public class ChildrenResponse : BaseResponse
+    {
+        public Child Child { get; set; }
+
+        public ChildrenResponse(Child item) : base(string.Empty, true)
+        {
+            this.Child = item;
+        }
+
+        public ChildrenResponse() : base(string.Empty, true, true)
+        {
+        }
+
+        public ChildrenResponse(string message) : base(message, false)
+        {
+        }
+
+        public ChildrenResponse(string message, bool authorise) : base(message, false, authorise)
+        {
+        }
+    }
+}
