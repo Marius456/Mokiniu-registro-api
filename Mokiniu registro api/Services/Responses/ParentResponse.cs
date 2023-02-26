c5fa92f6825ca45f5646c9ee4e7a50db7ba0e6c2
Marius <marius.zilguzis@ktu.edu>
2023-02-26 17:43:58 +0200

    added custom responses

---
+++ Mokiniu registro api/Services/Responses/ParentResponse.cs
@@ -0,0 +1,26 @@
+﻿using Mokiniu_registro_api.Models;
+
+namespace Mokiniu_registro_api.Services.Responses
+{
+    public class ParentResponse : BaseResponse
+    {
+        public Parent Parent { get; set; }
+
+        public ParentResponse(Parent item) : base(string.Empty, true)
+        {
+            this.Parent = item;
+        }
+
+        public ParentResponse() : base(string.Empty, true, true)
+        {
+        }
+
+        public ParentResponse(string message) : base(message, false)
+        {
+        }
+
+        public ParentResponse(string message, bool authorise) : base(message, false, authorise)
+        {
+        }
+    }
+}
